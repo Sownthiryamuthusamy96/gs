2ef12477d49baad5389933524fde4d33ad9bb616
Sownthiryamuthusamy96 <sownthiryamuthusamy@gmail.com>
2017-08-17 14:04:28 +0530

    Create Fib.java

---
+++ Fib.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class Fib
+{
+public class void main(String[] args)throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in());
+int i,f=0,n1,n2=0,n3=1;
+System.out.println(n2+" "+n3);
+for(i=2;i<=f;++i)
+{
+n1=n2+n3;
+n2=n3;
+n3=n1;
+}
+System.out.println(n1);
+}
+}
+
